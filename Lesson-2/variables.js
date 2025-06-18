06084e1bebeb8e7aba9b46296fb1eb6366f5d1f7
IvanSlivenko <ivan8822@ukr.net>
2025-06-18 21:10:03 +0300

    18-06-2025-21-09

---
+++ Lesson-2/variables.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(" lesson - 2");
+const myString = "Hello world";
+const myNumber = 42;
+console.log(myString);
+console.log(myNumber);
+console.log("---------------------------");
---
+++ Lesson-2/variables.ts
@@ -0,0 +1,12 @@
+console.log(" lesson - 2");
+
+const myString: string = "Hello world";
+const myNumber: number = 42;
+const myBoolean: Boolean = true;
+const undefinedVariable: undefined = undefined;
+const nullVariables: null = null;
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log("---------------------------");
